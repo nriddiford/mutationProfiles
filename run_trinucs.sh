6d9275e7b086b8e6563375eb4ee0e0a5a6009db2
nriddiford <nickriddiford@me.com>
2017-07-25 15:00:16 +0200

    Add -g flag for genome in run_trinucs

--- run_trinucs.sh
+++ run_trinucs.sh
@@ -5,19 +5,24 @@ usage() {
   usage:   run_trinucs.sh [options]
   options:
     -v    read from varscan native output
+    -g    path to genome.fasta
     -h    show this message
   "
 }
 
 varscan=0
 
-while getopts 'vh' flag; do
+genome=/Users/Nick_curie/Documents/Curie/Data/Genomes/dmel_6.12.fa
+
+while getopts 'vhg:' flag; do
   case "${flag}" in
     v)  varscan=1 ;;
+    g) genome=${OPTARG};;
     h)  usage
         exit 0 ;;
   esac
 done
+
 
 if [[ -f "data/GW.snv.dist.txt" ]]
 then
@@ -31,7 +36,7 @@ if [[ $varscan -eq 1 ]]
 then
   for varscan_file in data/*.snp
   do
-    perl script/trinucs.pl -g /Users/Nick_curie/Documents/Curie/Data/Genomes/dmel_6.12.fa -i $varscan_file
+    perl script/trinucs.pl -g $genome -i $varscan_file
   done
 fi
 
@@ -39,7 +44,7 @@ if [[ $varscan -eq 0 ]]
 then
   for vcf in data/*.vcf
   do
-    perl script/trinucs.pl -g /Users/Nick_curie/Documents/Curie/Data/Genomes/dmel_6.12.fa -v $vcf
+    perl script/trinucs.pl -g $genome -v $vcf
   done
 fi
 
