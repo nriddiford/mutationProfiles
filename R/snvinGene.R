7c3366a3b17daf06621025558517945724f444d0
nriddiford <nickriddiford@me.com>
2017-08-02 15:32:24 +0200

    Add tss line in snvingene plot

--- R/snvinGene.R
+++ R/snvinGene.R
@@ -7,14 +7,16 @@
 #' @keywords gene
 #' @export
 
-snvinGene <- function(gene_lengths="data/gene_lengths.txt",gene2plot='dnc'){
+snvinGene <- function(gene_lengths="data/gene_lengths.txt", gene2plot='dnc'){
   gene_lengths<-read.delim(gene_lengths, header = T)
   region<-filter(gene_lengths, gene == gene2plot)
+  
   wStart<-(region$start - 10000)
   wEnd<-(region$end + 10000)
   wChrom<-region$chrom
+  wTss<-suppressWarnings(as.numeric(levels(region$tss))[region$tss])
   data<-getData()
-  data<-filter(data, chrom == wChrom, pos >= wStart, pos <= wEnd)
+  data<-filter(data, chrom == wChrom & pos >= wStart & pos <= wEnd)
   
   if(nrow(data) == 0){
     stop(paste("There are no snvs in", gene2plot, "- Exiting", "\n"))
@@ -29,10 +31,11 @@ snvinGene <- function(gene_lengths="data/gene_lengths.txt",gene2plot='dnc'){
     )
   p<-p + scale_x_continuous("Mbs", expand = c(0,0), breaks = seq(round(wStart/1000000, digits = 2),round(wEnd/1000000, digits = 2),by=0.05), limits=c(wStart/1000000, wEnd/1000000))
   p<-p + annotate("rect", xmin=region$start/1000000, xmax=region$end/1000000, ymin=0, ymax=0.1, alpha=.2, fill="skyblue")
-  
+
+  p<-p + geom_segment(aes(x = wTss/1000000, y = 0, xend= wTss/1000000, yend = 0.1), colour="red")
   middle<-((wEnd/1000000+wStart/1000000)/2)
   p <- p + annotate("text", x = middle, y = 0.05, label=gene2plot, size=6)
   p<-p + ggtitle(paste("Chromosome:", wChrom))
-
+  
   p
 }
