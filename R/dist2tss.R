10cd2ff76622c54dc6a5b7b0e7f101eeabcd5599
nriddiford <nickriddiford@me.com>
2017-08-05 10:06:16 +0200

    dist2TSS stable

--- R/dist2tss.R
+++ R/dist2tss.R
@@ -8,33 +8,60 @@
 
 tssDist <- function(tss_pos="data/tss_positions.txt"){
   tss_locations<-read.delim(tss_pos, header = T)
+  tss_locations$tss<-as.integer(tss_locations$tss)
   data<-getData()
   
-  fun <- function(p) {
-    
-    index<-which.min(abs(tss_locations$tss - p))
-    closestTss<-tss_locations$tss[[index]]
-    dist<-(closestTss-p)
-    list(closest=closestTss, distance2nearest=dist)
+  #data<-filter(data, sample == "HUM-4")
+  #data<-droplevels(data)
+  
+  fun2 <- function(p) {
+    index<-which.min(abs(tss_df$tss - p))
+    closestTss<-tss_df$tss[index]
+    chrom<-as.character(tss_df$chrom[index])
+    gene<-as.character(tss_df$gene[index])
+    dist<-(p-closestTss)
+    list(p, closestTss, dist, chrom, gene)
   }
   
-  dist2tss<-lapply(data$pos, fun)
+  l <- list()
   
-  dist2tss<-do.call(rbind, dist2tss)
+  for (c in levels(data$chrom)){
+    df<-filter(data, chrom == c)
+    tss_df<-filter(tss_locations, chrom == c)
+    dist2tss<-lapply(df$pos, fun2)
+    dist2tss<-do.call(rbind, dist2tss)
+    dist2tss<-as.data.frame(dist2tss)
+    
+    colnames(dist2tss)=c("snp", "closest_tss", "min_dist", "chrom", "closest_gene")
+    dist2tss$min_dist<-as.numeric(dist2tss$min_dist)
+    l[[c]] <- dist2tss
+  }
+  
+  dist2tss<-do.call(rbind,l)
   dist2tss<-as.data.frame(dist2tss)
-  dist2tss<-arrange(dist2tss,(as.numeric(distance2nearest)))
+  dist2tss$chrom<-as.character(dist2tss$chrom)
   
-  dist2tss$distance2nearest<-as.numeric(dist2tss$distance2nearest)
+  dist2tss<-arrange(dist2tss,(abs(min_dist)))
+  
+  # SHould remove chroms with fewer than n observations (50?)
+  dist2tss<-filter(dist2tss, chrom != 4)
+  dist2tss<-filter(dist2tss, chrom != "Y")
+
+  #cols<-setCols(data, "chrom")
   
   p<-ggplot(dist2tss)
-  p<-p + geom_density(aes(distance2nearest), fill = "lightblue", alpha = 0.6)
-  p<-p + scale_x_continuous("Distance to TSS", limits=c(-10000, 10000))
+  p<-p + geom_density(aes(min_dist, fill = chrom), alpha = 0.3)
+  p<-p + scale_x_continuous("Distance to TSS (Kb)", limits=c(-20000, 20000), breaks=c(-10000, -1000, 0, 1000, 10000), expand = c(.0005, .0005), labels=c("-10", "-1", 0, "1", "10"))
+  p<-p + scale_y_continuous("Density", expand = c(0, 0))
+  p<-p + geom_vline(xintercept = 0, colour="black", linetype="dotted")
+  p<-p + cleanTheme()
+  #p<-p + facet_wrap(~chrom, ncol = 2, scales = "free_y")
   p
   
   # p<-ggplot(dist2tss)
-  # p<-p + geom_histogram(aes(distance2nearest), fill = "lightblue", alpha = 0.6, bins=500)
-  # p<-p + scale_x_continuous("Distance to TSS", limits=c(-10000, 10000))
+  # p<-p + geom_histogram(aes(min_dist, fill = chrom), alpha = 0.6, bins=500)
+  # p<-p + scale_x_continuous("Distance to TSS", limits=c(-1000, 1000))
+  # p<-p + geom_vline(xintercept = 0, colour="black", linetype="dotted")
   # p
   
-  #return(dist2tss)
 }
--- mutationProfiles.R
+++ mutationProfiles.R
@@ -379,9 +379,8 @@ tssDist <- function(tss_pos="data/tss_positions.txt"){
   tss_locations$tss<-as.integer(tss_locations$tss)
   data<-getData()
   
-  
-  data<-filter(data, sample == "HUM-4")
-  data<-droplevels(data)
+  #data<-filter(data, sample == "HUM-4")
+  #data<-droplevels(data)
   
   fun2 <- function(p) {
     index<-which.min(abs(tss_df$tss - p))
@@ -412,18 +411,19 @@ tssDist <- function(tss_pos="data/tss_positions.txt"){
   
   dist2tss<-arrange(dist2tss,(abs(min_dist)))
   
-  
+  # SHould remove chroms with fewer than n observations (50?)
+  dist2tss<-filter(dist2tss, chrom != 4)
+  dist2tss<-filter(dist2tss, chrom != "Y")
+
   #cols<-setCols(data, "chrom")
-  
-  #dummyDF<-head(dist2tss, 100)
   
   p<-ggplot(dist2tss)
   p<-p + geom_density(aes(min_dist, fill = chrom), alpha = 0.3)
-  p<-p + scale_x_continuous("Distance to TSS", limits=c(-100000, 100000))
+  p<-p + scale_x_continuous("Distance to TSS (Kb)", limits=c(-20000, 20000), breaks=c(-10000, -1000, 0, 1000, 10000), expand = c(.0005, .0005), labels=c("-10", "-1", 0, "1", "10"))
+  p<-p + scale_y_continuous("Density", expand = c(0, 0))
   p<-p + geom_vline(xintercept = 0, colour="black", linetype="dotted")
-  p<-p + facet_wrap(~chrom, scales = "free_y")
-  #p<-p + cols
-  
+  p<-p + cleanTheme()
+  #p<-p + facet_wrap(~chrom, ncol = 2, scales = "free_y")
   p
   
   # p<-ggplot(dist2tss)
