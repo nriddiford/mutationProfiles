239031c3591bfb5ad8085ceddf43a437524596ae
nriddiford <nickriddiford@me.com>
2017-08-07 14:02:45 +0200

    Add snvSim to simulate random SNVs accross genomic regions in 'data/intervals.bed'

--- R/dist2tss.R
+++ R/dist2tss.R
@@ -1,19 +1,27 @@
 #' tssDist
 #'
 #' Plot distance to TSS distribution
-#' @param tss_pos File containing "gene chrom tss" information [Default 'data/tss_positions.txt']
+#' @param tss_pos File containing all TSS positions in genome: "gene chrom tss" [Default 'data/tss_positions.txt']
+#' @param sim Simulate random SNVs accross genomic intervals? [Default: NO]
+#' @param print Write the simulated random SNVs to a bed file ('data/simulatedSNVs.bed')? [Default: NO]
 #' @import ggplot2
 #' @keywords tss
 #' @export
 
-tssDist <- function(tss_pos="data/tss_positions.txt"){
+tssDist <- function(tss_pos="data/tss_positions.txt",sim=NA, print=0){
   tss_locations<-read.delim(tss_pos, header = T)
   tss_locations$tss<-as.integer(tss_locations$tss)
-  data<-getData()
-  
-  #data<-filter(data, sample == "HUM-4")
-  #data<-droplevels(data)
-  
+  if(is.na(sim)){
+    data<-getData()
+  }
+  else{
+    cat("Generating simulated data\n")
+    data<-snvSim(N=1000, write=print)
+    colnames(data)<-c("chrom", "pos", "v3", "v4", "v5")
+    data<-filter(data, chrom == "2L" | chrom == "2R" | chrom == "3L" | chrom == "3R" | chrom == "X" | chrom == "Y" | chrom == "4")
+    data<-droplevels(data)
+  }
+
   fun2 <- function(p) {
     index<-which.min(abs(tss_df$tss - p))
     closestTss<-tss_df$tss[index]
@@ -37,21 +45,23 @@ tssDist <- function(tss_pos="data/tss_positions.txt"){
     l[[c]] <- dist2tss
   }
   
-  dist2tss<-do.call(rbind,l)
+  dist2tss<-do.call(rbind, l)
   dist2tss<-as.data.frame(dist2tss)
   dist2tss$chrom<-as.character(dist2tss$chrom)
   
   dist2tss<-arrange(dist2tss,(abs(min_dist)))
   
-  # SHould remove chroms with fewer than n observations (50?)
-  dist2tss<-filter(dist2tss, chrom != 4)
-  dist2tss<-filter(dist2tss, chrom != "Y")
+  # Removes chroms with fewer than 20 observations
+  snvCount <- table(dist2tss$chrom)
+  dist2tss <- subset(dist2tss, chrom %in% names(snvCount[snvCount > 20]))
 
-  #cols<-setCols(data, "chrom")
-  
   p<-ggplot(dist2tss)
   p<-p + geom_density(aes(min_dist, fill = chrom), alpha = 0.3)
-  p<-p + scale_x_continuous("Distance to TSS (Kb)", limits=c(-20000, 20000), breaks=c(-10000, -1000, 0, 1000, 10000), expand = c(.0005, .0005), labels=c("-10", "-1", 0, "1", "10"))
+  p<-p + scale_x_continuous("Distance to TSS (Kb)",
+                            limits=c(-100000, 100000),
+                            breaks=c(-10000, -1000, 0, 1000, 10000),
+                            expand = c(.0005, .0005),
+                            labels=c("-10", "-1", 0, "1", "10") )
   p<-p + scale_y_continuous("Density", expand = c(0, 0))
   p<-p + geom_vline(xintercept = 0, colour="black", linetype="dotted")
   p<-p + cleanTheme()
@@ -64,4 +74,32 @@ tssDist <- function(tss_pos="data/tss_positions.txt"){
   # p<-p + geom_vline(xintercept = 0, colour="black", linetype="dotted")
   # p
   
+}
+
+
+#' snvSim
+#'
+#' Generate simulated SNV hits acroos genomic regions (e.g. mappable regions)
+#' @param intervals File containing genomic regions within which to simulate SNVs [Default 'data/intervals.bed]
+#' @param N Number of random SNVs to generate [Default 1000]
+#' @param write Write the simulated random SNVs to a bed file ('data/simulatedSNVs.bed')? [Default: NO]
+
+#' @import GenomicRanges
+#' @keywords sim
+#' @export
+
+snvSim <- function(intervals="data/intervals.bed", N=1000, write=F){
+  suppressPackageStartupMessages(require(GenomicRanges))
+
+  intFile <- import.bed(intervals)
+  space <- sum(width(intFile))
+  positions <- sample(c(1:space), N)
+  new_b <- GRanges(seqnames=as.character(rep(seqnames(intFile), width(intFile))),
+                   ranges=IRanges(start=unlist(mapply(seq, from=start(intFile), to=end(intFile))), width=1))
+  bedOut<-new_b[positions]
+  if(write){
+    export.bed(new_b[positions], "data/simulatedSNVs.bed")
+  }
+  remove(new_b)
+  return(data.frame(bedOut))
 }
