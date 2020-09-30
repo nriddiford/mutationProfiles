640780c3aeb4d8d564b20b355c479219dc82748d
nriddiford <nickriddiford@me.com>
2020-09-30 11:30:41 +0200

    Allow config spec

--- script/run_filtervcf.sh
+++ script/run_filtervcf.sh
@@ -6,6 +6,7 @@ usage() {
   options:
     -d    VCF directory
     -p    panel of normals
+    -c    config
     -o    output directory
     -h    show this message
   "
@@ -14,20 +15,21 @@ usage() {
 dir=$(pwd)
 panel_of_normals=/Volumes/perso/Analysis/Analysis/Mutect2/panel_of_normals.vcf.gz
 output_dir=$(pwd)
+config=data/samples.txt
 
-
-while getopts 'd:p:o:' flag; do
+while getopts 'd:p:d:o:' flag; do
   case "${flag}" in
-    s)  dir=${OPTARG};;
-    f)  panel_of_normals=${OPTARG};;
+    d)  dir=${OPTARG};;
+    p)  panel_of_normals=${OPTARG};;
+    c)  config=${OPTARG};;
     o)  output_dir=${OPTARG};;
     h)  usage
     exit 0 ;;
   esac
 done
 
-echo "python script/filtervcf.py -d $dir --panel-of-normals $panel_of_normals"
-python script/filtervcf.py -d $dir --panel-of-normals $panel_of_normals
+echo "python script/filtervcf.py -d $dir --panel-of-normals $panel_of_normals --config $config"
+python script/filtervcf.py -d $dir --panel-of-normals $panel_of_normals --config $config
 
 # if [ -d $dir ]
 # then
